b5425f07765014a2c33f6b4575da65b0cfe3f88f
Marcelo Camargo <marcelo.camargo@ngi.com.br>
2016-09-07 02:57:20 -0300

    Resolve troubles

--- functions/git-loc.fish
+++ functions/git-loc.fish
@@ -6,6 +6,6 @@
 #
 
 function git-loc -d "Git lines of code"
-    git ls-files | xargs wc -l | awk '{$$1=$$1}1;' | sort -t '_' -k 1n | sed -E "s/([0-9]+) (.*)/| \1 - \2/g" | column
+    git ls-files | xargs wc -l | sort -t '_' -k 1n | sed -E "s/([0-9]+) (.*)/| \1 - \2/g" | awk '{$1=$1}1;' | column
 end
 
